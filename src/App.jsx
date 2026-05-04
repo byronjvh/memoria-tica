a7b505e91a650548a6948dce4096e36a79275700
Byron Vargas <byronjvh02@gmail.com>
2026-05-03 22:50:07 -0600

    UPD: hero style

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,17 @@ function App() {
     <>
       <div className='hero ' onClick={clickOutSide}>
         <div className='flex max-w-270 mx-auto pt-10 px-2 gap-4'>
-          <div className='flex-1 max-w-104 max-h-125 flex flex-col pt-4 gap-4' >
-            <h1 className='text-balance text-white'>
-              Descubre las memorias visuales de Costa Rica
-            </h1>
-            <p className='text-gray-700'>
-              Imágenes, lugares y memorias organizadas por provincia y cantón.
-            </p>
-            <div className='bg-white p-2 rounded-md flex flex-col gap-2'>
-              <select name="" id="" className='border border-gray-300 w-full rounded'>
+          <div className='flex-1 max-w-104 max-h-125 flex flex-col pt-4 gap-6' >
+            <div className='flex flex-col gap-2'>
+              <h1 className='text-balance text-white'>
+                Descubre las memorias visuales de Costa Rica
+              </h1>
+              <p className='text-gray-700 text-lg'>
+                Imágenes, lugares y memorias organizadas por provincia y cantón.
+              </p>
+            </div>
+            <div className='bg-white p-0.5 rounded-md flex flex-col gap-2'>
+              <select name="" id="" className='border-2 border-sky-300 w-full rounded p-2 cursor-pointer'>
                 <option value="0">Selecciona una provincia</option>
                 <option value="1">San José</option>
                 <option value="2">Alajuela</option>
@@ -37,11 +39,13 @@ function App() {
                 <option value="6">Puntarenas</option>
                 <option value="7">Limón</option>
               </select>
-              <select name="" id="" className='border border-gray-300 w-full rounded'>
+            </div>
+            <div className='bg-white p-0.5 rounded-md flex flex-col gap-2'>
+              <select name="" id="" className='border-2 border-sky-300 w-full rounded p-2 cursor-pointer'>
                 <option value="0">Selecciona un cantón</option>
               </select>
             </div>
-            <button className='w-max flex items-center gap-1 font-bold px-4 py-3 bg-orange-400 rounded-md uppercase cursor-pointer border-2 border-white text-white hover:-translate-y-0.5 transition-transform duration-200 ease-out'>Explorar museo digital <Arrow size={22} /></button>
+            <button className='w-max hover:brightness-110 text-sm flex items-center gap-1 font-bold px-4 py-3 bg-orange-400 rounded-md uppercase cursor-pointer border-2 border-white text-white hover:-translate-y-0.5 transition duration-200 ease-out'>Explorar museo digital <Arrow size={22} /></button>
           </div>
           <div className='flex-1 max-w-125 max-h-125 mx-auto overflow-hidden flex justify-center items-center'>
             <Map updateSelected={updateSelectedProvince} selectedProvince={selectedProvince} />
