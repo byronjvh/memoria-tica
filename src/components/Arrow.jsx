f2d0a46b3ba8dbe825f2d67724bcb5376613d1f0
Byron Vargas <byronjvh02@gmail.com>
2026-05-03 19:41:49 -0600

    add: Arrow icon for button

---
+++ src/components/Arrow.jsx
@@ -0,0 +1,7 @@
+export default function Arrow({ size = 24 }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none">
+            <path d="M4 12H20M20 12L16 8M20 12L16 16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
